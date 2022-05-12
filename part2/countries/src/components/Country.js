8fda9396677f822bba894e65e681a6c93a358a06
Nina <labferment10@gmail.com>
2022-05-12 14:05:21 +0200

    display weather for capital

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,11 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const County = (props) => {
+	const [weatherInfo, setWeatherInfo] = useState(null);
 	const {
 		name: { common },
 		capital,
 		area,
 		languages,
 		flags,
+		latlng: [lat, lon],
 	} = props;
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
+			)
+			.then((response) => response.data)
+			.then((data) => {
+				setWeatherInfo(data);
+				console.log(data);
+			});
+	}, []);
+
 	return (
 		<div>
 			<h1>{common}</h1>
@@ -21,6 +39,13 @@ const County = (props) => {
 				))}
 			</ul>
 			<img src={flags.png} alt={`${common}'s flag`} />
+			<h2>Weather in {capital}</h2>
+			<p>temperature {weatherInfo?.main.temp}&#8451;</p>
+			<img
+				src={`http://openweathermap.org/img/wn/${weatherInfo?.weather[0].icon}@2x.png`}
+				alt='icon'
+			/>
+			<p>wind {weatherInfo?.wind?.speed} m/s</p>
 		</div>
 	);
 };
