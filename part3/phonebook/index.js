0584b13f8af5269f36f633e8aefdeeb6fd77c44a
Nina <labferment10@gmail.com>
2022-05-20 19:09:29 +0200

    add step1 and step2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+const persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+];
+
+// step1
+app.get('/api/persons', (request, response) => {
+	response.json(persons);
+});
+
+// step2
+app.get('/info', (request, response) => {
+	response.send(`
+	<p>Phonebook has info for ${persons.length} people</p>
+	<p>${new Date().toUTCString()}</p>
+	`);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
