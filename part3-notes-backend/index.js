42ef16ad71e4a31404dfaaf2fb0b94aa28e4a0ee
Nina <labferment10@gmail.com>
2022-06-27 13:58:08 +0200

    update part3-notes-backend

--- part3-notes-backend/index.js
+++ part3-notes-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 let notes = [
 	{
@@ -33,6 +34,10 @@ const requestLogger = (request, response, next) => {
 app.use(express.json());
 
 app.use(requestLogger);
+
+app.use(cors());
+
+app.use(express.static('build'));
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
@@ -92,7 +97,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
