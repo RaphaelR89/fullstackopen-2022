9029ee45980534f5877682a0d69dc29bf125ca6e
Nina <labferment10@gmail.com>
2022-06-28 15:17:26 +0200

    add procfile

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
@@ -114,7 +116,7 @@ app.post('/api/persons', (request, response) => {
 	response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
